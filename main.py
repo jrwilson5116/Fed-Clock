0007a8766d91b454370e6e20dd51d7e5a8f098ef
unknown <jrwilson5116@gmail.com>
2018-02-14 18:12:26 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,114 @@
+from time_keeper import get_time_decimal, over_under,get_minutes
+from flask import Flask, request
+
+
+
+app = Flask(__name__)
+app.config['DEBUG']=True
+
+
+form = """
+<!DOCTYPE html>
+
+
+
+<html>
+
+    <head>
+        <header>
+            <h1> Fed Clock  </h1>
+        </header>
+        <style>
+         
+            form {{
+
+                background-color: #eee;
+
+                padding: 20px;
+
+                margin: 0 auto;
+
+                width: 540px;
+
+                font: 16px sans-serif;
+
+                border-radius: 10px;
+
+            }}
+
+            h1 {{
+                font: 80px sans-serif;
+                
+                padding: 20px;
+
+                margin: 0 auto;
+
+                width: 540px;
+
+                border-radius: 10px;
+
+                color: #483D8B
+
+
+            }}
+
+            footer {{
+                padding: 20px;
+
+                margin: 0 auto;
+
+                width: 540px;
+
+                border-radius: 10px;
+            }}
+
+        </style>
+
+    </head>
+
+    <body>
+
+		<form action = "/" method = "post">
+			Time in: 
+			<input name ="time_in" type ="text" value = 0>
+            Hours Worked:
+            <input name = "time_worked" type = "text" value = {0}>
+			<br><br>
+            Time out:
+			<input name = "time_out" type ="text" value = 0>
+            Over/Under:
+            <input name = "over_under" typer = "text" value = {1}>
+			<br><br>
+			<input type ="submit">
+            <input type = "reset" value = "Clear fields">
+    
+		</form>
+
+
+    </body>
+    <footer>
+    version 1.0<br>
+    created by: Joey Wilson
+    </footer>
+</html>
+    """
+
+@app.route("/")
+def index():
+    return form.format("","")
+
+
+@app.route("/",methods = ['POST'])
+def calculate():
+    clock_in= request.form['time_in']
+    clock_out=request.form['time_out']
+    total = get_time_decimal(clock_in,clock_out)
+    over = over_under(total)
+    return form.format(total,over)
+
+
+
+
+
+if __name__=="__main__":
+    app.run()
---
+++ time_keeper.py
@@ -0,0 +1,74 @@
+import random
+
+def format_time(time):
+    pm = False
+    length = len(time)
+    for i in range(0,length):
+        if not time[i].isdigit():
+            if time[i].lower()=="p":
+                pm = True
+            time = time.replace(time[i]," ")
+    time = time.replace(" ","")
+    if pm and int(time)<1200:
+        time = int(time) + 1200
+    return int(time)
+
+
+def get_minutes(military_time):
+    minutes = military_time // 100 * 60
+    minutes = minutes + military_time % 100
+    return minutes
+
+
+def get_time_decimal(clock_in,clock_out):
+    try:
+        clock_in = format_time(clock_in)
+        clock_out = format_time(clock_out)
+    except ValueError:
+        return "Invalid input"
+    if clock_in > clock_out:
+        return "Invalid input"
+    time_worked = get_minutes(clock_out) - get_minutes(clock_in)
+    extraneous = time_worked % 6
+    time_worked = time_worked - extraneous
+    decimal = time_worked // 60
+    decimal = decimal + time_worked % 60 / 60
+    return decimal 
+
+
+def over_under(time):
+    try:
+        if time <= 6:
+            time = time * 10
+            time = time - 80
+            time = time / 10
+        else:
+            time = time * 10
+            time = time - 85
+            time = time / 10
+        return time
+    except ValueError:
+        return "Invalid input!"
+
+
+
+def main():
+    print("#################################################")
+    print("testing with random numbers")
+    print("#################################################")
+    for i in range(0,101):
+        time_in = random.randint(6,12) * 100 + random.randint(0,60)
+        time_out = random.randint(13,16) * 100 + random.randint(0,60)
+        decimal = get_time_decimal(time_in,time_out)
+        print("clocked in: "+str(time_in)+" clocked out: "+str(time_out)+ " decimal time: "+str(decimal)+" Over: "+str(over_under(decimal)))
+        
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
