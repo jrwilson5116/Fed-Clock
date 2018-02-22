a71dea944fa5cd2055f406eadc5d57dd8cf56f02
unknown <jrwilson5116@gmail.com>
2018-02-21 19:29:43 -0600

    trashed form, no need for it anymore

--- main.py
+++ main.py
@@ -2,96 +2,9 @@ from time_keeper import get_time_decimal, over_under,get_minutes
 from flask import Flask, request, render_template
 
 
-
 app = Flask(__name__)
 app.config['DEBUG']=True
 
-
-form = """
-<!DOCTYPE html>
-
-
-
-<html>
-
-    <head>
-        <header>
-            <h1> Fed Clock  </h1>
-        </header>
-        <style>
-         
-            form {{
-
-                background-color: #eee;
-
-                padding: 20px;
-
-                margin: 0 auto;
-
-                width: 540px;
-
-                font: 16px sans-serif;
-
-                border-radius: 10px;
-
-            }}
-
-            h1 {{
-                font: 80px sans-serif;
-                
-                padding: 20px;
-
-                margin: 0 auto;
-
-                width: 540px;
-
-                border-radius: 10px;
-
-                color: #483D8B
-
-
-            }}
-
-            footer {{
-                padding: 20px;
-
-                margin: 0 auto;
-
-                width: 540px;
-
-                border-radius: 10px;
-            }}
-
-        </style>
-
-    </head>
-
-    <body>
-
-		<form action = "/" method = "post">
-			Time in: 
-			<input name ="time_in" type ="text" value = 0>
-            Hours Worked:
-            <input name = "time_worked" type = "text" value = {0}>
-			<br><br>
-            Time out:
-			<input name = "time_out" type ="text" value = 0>
-            Over/Under:
-            <input name = "over_under" typer = "text" value = {1}>
-			<br><br>
-			<input type ="submit">
-            <input type = "reset" value = "Clear fields">
-    
-		</form>
-
-
-    </body>
-    <footer>
-    version 1.1<br>
-    created by: Joey Wilson
-    </footer>
-</html>
-    """
 
 @app.route("/")
 def index():
