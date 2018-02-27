0eaeac6cf5dbe96df23ae63303b8ce82a35d7b19
jrwilson5116 <jrwilson5116@gmail.com>
2018-02-26 20:24:33 -0600

    adds flash error messages

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from time_keeper import get_time_decimal, over_under,format_time
-from flask import Flask, request, render_template,redirect,url_for
+from flask import Flask, request, render_template, redirect,url_for,flash
 
 
 app = Flask(__name__)
 app.config['DEBUG']=True
+app.secret_key='abcdefg'
 
 
 @app.route("/")
@@ -13,10 +14,10 @@ def index():
 
 @app.route("/",methods = ['POST'])
 def calculate():
-    error = None
     clock_in = request.form['time_in']
     clock_out = request.form['time_out']
     if format_time(clock_out) < format_time(clock_in):
+        flash("Invalid times, clock out must come AFTER clock in")
         return redirect(url_for('index'))
     else:
         total = get_time_decimal(clock_in,clock_out)
